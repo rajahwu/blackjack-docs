8335473aea40caff0b93e1786f6083ce0a9fc118
rajahwu <vmradford@gmail.com>
2025-10-28 11:39:19 -0400

    fix: fixed mobile overflow issue, temp fix

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      width: {
+        screen: '100%', // fixes mobile viewport overflow
+      },
+    },
+  },
+};
